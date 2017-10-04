ae9b82b1848e91ab96af19f44a71e9ee4d16cf9b
yeniree@gmail.com <yeniree@gmail.com>
2017-10-04 17:31:32 -0300

    Test WS Rest

---
+++ WebApplicationHolaMundoRest/src/java/edu/curso/ejemplo/rest/HolaMundoRest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.curso.ejemplo.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author educacionit
+ */
+@Path("holamundo")
+public class HolaMundoRest {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of HolaMundoRest
+     */
+    public HolaMundoRest() {
+    }
+
+    /**
+     * Retrieves representation of an instance of edu.curso.ejemplo.rest.HolaMundoRest
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String saludar() {
+        return "Hola Mundo";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HolaMundoRest
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void saludarEnviandoDatos(String content) {
+        System.out.println("Contenido: " + content);
+    }
+}
